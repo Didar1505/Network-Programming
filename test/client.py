926121e685b8e501151a6d6a9956848f415f5ea4
Didar1505 <didarkamiljanow15@gmail.com>
2025-11-11 16:28:07 +0300

    Minor adjustment

--- test/client.py
+++ test/client.py
@@ -1,45 +1,16 @@
 import socket
-import sys
-import threading
-import random
 
-nickname = f"User-{random.randint(1,100)}"
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 12000))
+print("connected to the server")
 
-def handle_messages(client:socket.socket):
-    while True:
-        try:
-            message = client.recv(1024)
-            if message:
-                if message.decode() == "NICK":
-                    client.send(nickname.encode())
-                else:
-                    sys.stdout.write('\r' + ' ' * 80 + '\r')  # Clear line
-                    print(message.decode())
-                    sys.stdout.write("You: ")  # Reprint prompt
-                    sys.stdout.flush()
-        except Exception as e:
-            print("Error occurred during connection:", e)
-            client.close()
-            break
+file_path = 'downloaded/test.txt'
+file = open(file_path, 'wb')
 
-def main():
-    try:
-        client  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(("127.0.0.1", 12000))
-        print("Connected to the server!")
-        threading.Thread(target=handle_messages, args=(client,)).start()
-
-        while True:
-            message = input("You: ")    
-            if message == "quit":
-                break
-            client.send(message.encode())
-
-
-    except Exception as e:
-        print("Error ocurred with client: ", e)
-    finally:
-        client.close()
-        print("connection closed")
-
-main()
+while True:
+    data = client.recv(1024)
+    file.write(data)
+    if not data: break
+file.close()
+client.close()
+print("Downloaded file")
--- test/server.py
+++ test/server.py
@@ -1,87 +1,19 @@
 import socket
-import threading
-from weather import Weather
-from quote import Quote
 
-connections = []
-nicknames = []
+filename = 'test.txt'
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 12000))
+server.listen(5)
 
-def broadcast(message, client=None):
-    for conn in connections:
-        if client != conn:
-            conn.send(message)
+file = open(filename, 'rb')
+line = file.read(1024)
 
-def remove_client(client:socket.socket):
-    if client in connections:
-        index = connections.index(client)
-        nick = nicknames[index]
-        print(f"{nick} left the chat")
-        nicknames.remove(nick)
-        client.close()
-        connections.remove(client)
+conn, adr = server.accept()
+print("Started server")
 
-
-def handle_command(raw, client:socket.socket):
-    raw = raw.split()
-    command = raw[0]
-    if command == '/weather':
-        temperature = Weather().get_weather(raw[1])
-        response = f"The weather in {raw[1]} is {str(temperature)} temp"
-        client.send(response.encode())
-    if command == '/quote':
-        response = Quote().get_random_quote()
-        message_send = f'{response["quote"]} - {response['author']}'
-        client.send(message_send.encode())
-
-
-def handle(client:socket.socket):
-    while True:
-        try:
-            message = client.recv(1024)
-            if message:
-                message = message.decode()
-                if message.startswith("/"):
-                    handle_command(message, client)
-                else:
-                    index = connections.index(client)
-                    nick = nicknames[index]
-                    message_send = f"{nick}: {message}"
-                    print(message_send)
-                    broadcast(message_send.encode(), client)
-            else:
-                remove_client(client)
-        except Exception as e:
-            print(f"Error to hande the connection: ", e)
-            remove_client(client)
-            break
-
-def main():
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('0.0.0.0', 12000))
-        server.listen(10)
-        print("Server started listening on port:", 12000)
-
-
-        while True:
-            client, address = server.accept()
-            client.send("NICK".encode())
-            nick = client.recv(1024).decode()
-            nicknames.append(nick)
-            print(f"Connected {nick} with address: {address}")
-            
-            connections.append(client)
-            threading.Thread(target=handle, args=(client,), daemon=True).start()
-
-
-    except Exception as e:
-        print("An error ocurred:",e)
-    finally:
-        if len(connections) > 0:
-            for conn in connections:
-                remove_client(conn)
-        server.close()
-
-if __name__ == '__main__':
-    threading.Thread(target=main, daemon=True).start()
-    input("Press Enter to exit! \n")
+while line:
+    conn.send(line)
+    line = file.read(1024)
+file.close()
+conn.close()
+server.close()
